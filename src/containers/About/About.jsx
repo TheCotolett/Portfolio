97ece2c2cdb756bccfe1d086ce4e7c89d778b164
Lorenzo <lorenzotamburrini2@gmail.com>
2022-07-24 12:49:31 +0200

    Second Update

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import './About.css'
 
 function About() {
   return (
-    <div>About</div>
+    <div className='about-container'>
+      <h1>About</h1>
+    </div>
   )
 }
 
