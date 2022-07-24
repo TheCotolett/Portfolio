2e8cd2d115902b4fabdae2a6de7d946dc5f144d1
Lorenzo <lorenzotamburrini2@gmail.com>
2022-07-24 19:03:29 +0200

    Update

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,10 +2,18 @@ import logo from '../assets/logo.png';
 import spotify from '../assets/spotify.png';
 import javascript from '../assets/javascript.png';
 import bgIMG from '../assets/bgIMG.png';
+import about01 from '../assets/about01.png';
+import about02 from '../assets/about02.png';
+import about03 from '../assets/about03.png';
+import about04 from '../assets/about04.png';
 const files = {
     logo,
     spotify,
     javascript,
-    bgIMG
+    bgIMG,
+    about01,
+    about02,
+    about03,
+    about04
 }
 export default files;
--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import './About.css'
+import { motion } from 'framer-motion'
+import { images } from '../../assets/images'
 
 function About() {
+  const about = [
+    {
+      id: '1',
+      desc: 'I am a quick learner with a passion for coding and problem solving',
+      img: '',
+    },
+    {
+      id: '2',
+      desc: 'Interested in sports, DJing and technology',
+      img: '',
+    },
+    {
+      id: '3',
+      desc: 'Gratuated from high school with a degree in computer science with 100/100',
+      img: '',
+    },
+  ]
   return (
     <div className='about-container'>
-      <h1>About</h1>
+      <h1>About me</h1>
+      //map through about array and display each item
+      {about.map((item) => (
+        <motion.div
+          key={item.id}
+          className='about-item'
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          whileHover={{ scale: 1.2 }}
+          style={{x:100}}
+          durantion={10}
+        >
+          <img src={'../../assets/about1.png'} alt='about' />
+          <p>{item.desc}</p>
+        </motion.div>
+      ))}
     </div>
   )
 }
