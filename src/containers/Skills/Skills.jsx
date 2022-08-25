de8d1cc91ac291035cf06a6eec7a4b890b237294
Lorenzo <lorenzotamburrini2@gmail.com>
2022-08-25 16:45:54 +0200

    WIP

--- src/containers/Skills/Skills.jsx
+++ src/containers/Skills/Skills.jsx
@@ -6,14 +6,19 @@ import './Skills.css'
 
 function Skills() {
   const [skills, setSkills] = useState([]);
+  const [projects, setProjects] = useState([]);
   useEffect( () =>{
     const skills_query = '*[_type == "skills"]';
     client.fetch(skills_query)
     .then((data) =>{
       setSkills(data);
     })
+    const projects_query = '*[_type == "projects"]';
+    client.fetch(projects_query)
+    .then((data) =>{
+      setProjects(data);
+    })
     
-
   }, [])
 
   return (
@@ -37,6 +42,34 @@ function Skills() {
             ))
           }
         </motion.div>
+      </div>
+
+      <div className='projects-wrapper'>
+          <motion.div className='projects-list'>
+            {
+              projects.map( (project) => (
+                <motion.div
+                  key={project.name}
+                  whileInView={{opacity: [0, 1]}}
+                  transition={{duration: 0.5}}
+                  className="project"
+                >
+                  <h2> {project.name} </h2>
+                  <img className='project-icon' src={urlFor(project.image)} alt="project" />
+
+                  <motion.div
+                    className='project-links-mdiv'
+                    whileHover={{opacity: [0,1]}}
+                    transition={{duration: 1}}
+                  >
+                    <a href={project.app}></a>
+                    <a href={project.link}></a>
+                  </motion.div>
+
+                </motion.div>
+              ))
+            }
+          </motion.div>
       </div>
 
     </div>
