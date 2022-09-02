61c776085b764e14a2ea4469b39f5795321d1c13
TheCotolett <lorenzotamburrini2@gmail.com>
2022-09-02 11:34:26 +0200

    NavBar link not working

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Navbar } from './components';
 function App() {
   return (
     <div className="App">
-      {/* <Navbar /> */}
+      <Navbar />
       <Header />
       <About />
       <Skills />
