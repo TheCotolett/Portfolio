8915bd7a0ede47ae1b4cd8b276f006f9789e53f4
TheCotolett <lorenzotamburrini2@gmail.com>
2022-09-06 16:57:23 +0200

    Contact button finished

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,32 +1,46 @@
-import {React, useState} from 'react'
-import './Contact.css'
-import {FiMail} from 'react-icons/fi'
-import {GrFormClose} from 'react-icons/gr'
+import { React, useState } from "react";
+import "./Contact.css";
+import { FiMail } from "react-icons/fi";
+import { GrFormClose } from "react-icons/gr";
+import { MdAlternateEmail } from "react-icons/md";
+import { images } from '../../constants'
+
 
 function Contact() {
-    const [isContactOpen, setIsContactOpen] = useState(false);
+  const [isContactOpen, setIsContactOpen] = useState(false);
 
-    if(isContactOpen === false) {
-        return (
-            <div className='contact-container' onClick={ ()=> {setIsContactOpen(true)} }>
-                <div className='contact-circle'>
-                    <FiMail size={40}/>
-                </div>
-            </div>
-        )
-    } else {
-        return (
-            <div className="contact-container">
-                <div className='contact-box'>
-                    <div className="contact-box-header">
-                        <p> Contact me </p>
-                        <GrFormClose onClick={ ()=> {setIsContactOpen(false)} }/>
-                    </div>
-                    <p> Hi </p>
-                </div>
-            </div>
-        )
-    }
+  if (isContactOpen === false) {
+    return (
+      <div
+        className="contact-container"
+        onClick={() => {
+          setIsContactOpen(true);
+        }}
+      >
+        <div className="contact-circle">
+          <FiMail size={40} />
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="contact-container">
+        <div className="contact-box">
+          <div className="contact-box-header">
+            <h3> Contact me </h3>
+            <GrFormClose
+              onClick={() => {
+                setIsContactOpen(false);
+              }}
+            />
+          </div>
+          <p> Mail: lorenzotamburrini2@gmail.com </p>
+          <p> Linkedin: Lorenzo Tamburrini </p>
+          <a href={images.CV_Tamburrini_Lorenzo} target="_blank" download> Download my CV </a>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Contact
+export default Contact;
